e4ad43afcf15f4ac813ec47bba715dc53a5ce0bf
zhangjianyong <tearflyinheaven@gmail.com>
2014-12-16 00:30:29 +0800

    关闭链接

--- src/com/doumiao/joke/schedule/spider/TextXiaohuaZol.java
+++ src/com/doumiao/joke/schedule/spider/TextXiaohuaZol.java
@@ -2,8 +2,6 @@ package com.doumiao.joke.schedule.spider;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 
@@ -67,9 +65,16 @@ public class TextXiaohuaZol {
 	public List<Article> fetch(String url) throws Exception {
 		HttpClient client = HttpClientHelper.getClient();
 		HttpGet get = new HttpGet(url);
-		HttpResponse response = client.execute(get);
-		Document listDoc = Jsoup.parse(EntityUtils.toString(
-				response.getEntity(), "utf-8"));
+		Document listDoc = null;
+		try {
+			HttpResponse response = client.execute(get);
+			listDoc = Jsoup.parse(EntityUtils.toString(response.getEntity(),
+					"utf-8"));
+		} catch (Exception e) {
+			log.error(e, e);
+		} finally {
+			get.releaseConnection();
+		}
 		Elements es = listDoc.select("li.article-summary");
 		List<Article> l = new ArrayList<Article>(es.size());
 		for (int i = 0; i < es.size(); i++) {
