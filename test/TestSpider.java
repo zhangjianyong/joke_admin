ec84d2d666184051c5a25f06650bf8107a62c95d
weizhiliang@gmail.com <Danawa1234>
2015-05-26 19:38:40 +0800

    测试

--- test/TestSpider.java
+++ test/TestSpider.java
@@ -1,8 +1,15 @@
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 import com.doumiao.joke.lang.Article;
+import com.doumiao.joke.lang.HttpClientHelper;
 import com.doumiao.joke.schedule.spider.AshamedQiushibaike;
 import com.doumiao.joke.schedule.spider.Pic0824;
 import com.doumiao.joke.schedule.spider.PicJuyouqu;
@@ -73,6 +80,22 @@ public class TestSpider {
 		List<Article> articles = spider.fetch("http://www.qiushibaike.com/late/page/1");
 		for (Article article : articles) {
 			System.out.println(article.toString());
+		}
+	}
+	@Test
+	public void testAlima() throws Exception {
+		HttpClient client = HttpClientHelper.getClient();
+		HttpGet get = new HttpGet("http://ai.taobao.com/auction/edetail.htm?e=D4VOKgmnuya6k0Or%2B%2BH4tF8JomzViWxGnnDOe2U0TNmLltG5xFicOcyvbMN%2FjluyDPIwxrc30riHmuQ7qhehfguw8cKeRvyhtuL%2BTYxC3iphnBGBH8ep6lFE%2BBP3Qa7QUa6whSeFsYBt2myTO7A4Ns6l7saVk%2BlHzz9qK%2BVhXaI%3D&ptype=100010&unid=150i2060eTBKGWzc10F100000&from=basic&clk1=3d6c63af274c09d4e8239df0df52bf84&upsid=3d6c63af274c09d4e8239df0df52bf84");
+		Document listDoc = null;
+		try {
+			HttpResponse response = client.execute(get);
+			listDoc = Jsoup.parse(EntityUtils.toString(response.getEntity(),
+					"utf-8"));
+			System.out.println(listDoc);
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			get.releaseConnection();
 		}
 	}
 }
